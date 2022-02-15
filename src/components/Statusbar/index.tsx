ade8a822d6bf6cb89a9c74d0426438dc6e0a57e2
Fernando Dias <56140193+nand0diaz@users.noreply.github.com>
2022-02-15 15:11:12 -0300

    status bar

--- src/components/Statusbar/index.tsx
+++ src/components/Statusbar/index.tsx
@@ -1,13 +1,48 @@
-import React from 'react'
-import { Box } from '@chakra-ui/react'
+import React, { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+import { Box, VStack, Center } from '@chakra-ui/react'
 import { Statusbar as StatusbarSvg } from '../../assets/svgs/Statusbar'
 
 export const Statusbar: React.FC = () => {
-  // check which page it is in
+  const [location, setLocation] = useState('')
+  const [step, setStep] = useState(Number)
+
+  useEffect(() => {
+    const path = useLocation()
+    const { pathname } = path
+
+    setLocation(pathname)
+  }, [])
+
+  useEffect(() => {
+    location === '/register'
+      ? setStep(1)
+      : location === '/zipCode' ||
+        location === '/zipCode/successful' ||
+        location === '/zipCode/unavailable'
+      ? setStep(3)
+      : location === '/installation'
+      ? setStep(5)
+      : location === '/checkout'
+      ? setStep(7)
+      : location
+  }, [location])
 
   return (
-    <Box>
+    <VStack pb={step !== undefined ? '32px' : '0'}>
       <StatusbarSvg step={step} />
-    </Box>
+      {step === 1 ||
+        (step === 3 && (
+          <Center>
+            <Box textStyle="priceText">Dados Pessoais</Box>
+          </Center>
+        ))}
+      {step === 6 ||
+        (step === 7 && (
+          <Box textStyle="priceText" right="0">
+            Contratar
+          </Box>
+        ))}
+    </VStack>
   )
 }
