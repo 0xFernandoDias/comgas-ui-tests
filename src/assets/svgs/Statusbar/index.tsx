4698ee331da75bb4962cafa2a7f0e2114b7c6538
Alexandre Matheus <alexandre@labsit.io>
2022-02-18 15:28:48 -0300

    [ADD] Aplicando modelo dinamico para o status bar

--- src/assets/svgs/Statusbar/index.tsx
+++ src/assets/svgs/Statusbar/index.tsx
@@ -5,9 +5,9 @@ import {Step4} from './Step4'
 
 const steps = new Map()
 
-steps.set("1", Step1)
-steps.set("2", Step2)
-steps.set("3", Step3)
-steps.set("4", Step4)
+steps.set('1', Step1)
+steps.set('2', Step2)
+steps.set('3', Step3)
+steps.set('4', Step4)
 
 export { steps }
